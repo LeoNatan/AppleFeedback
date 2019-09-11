c0acd6b1f89ec89a555c18021cf3e87775879d57
Leo Natan <leo.natan@outlook.com>
2019-09-11 08:32:09 +0300

    Demo project of FB7199451

---
+++ FB7199451/FB7199451/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FB7199451
+//
+//  Created by Leo Natan (Wix) on 9/11/19.
+//  Copyright © 2019 LeoNatan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FB7199451/FB7199451/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  FB7199451
+//
+//  Created by Leo Natan (Wix) on 9/11/19.
+//  Copyright © 2019 LeoNatan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+
+@end
